84051618182cbc62b6b1fb2ed6b0d46229dc8239
Noirpuppet <chenin2808@gmail.com>
2022-12-01 22:51:45 +0530

    created a function for operand and operator

--- cal.js
+++ cal.js
@@ -15,18 +15,114 @@ function displayTime(){
 }
 setInterval(displayTime, 10);
 
-const screen = document.querySelector(".screen");
-const buttons = document.querySelectorAll('.btn');
+
+////////////////////////////////////////////////////
+
+let displayValue = "";
+let firstInput = "";
+let secondInput = "";
+let operator = "";
+
+let display = document.getElementById('screen');
+let numbers = document.querySelectorAll('.num');
+let operators = document.querySelectorAll('.sign');
+let root = document.querySelector('.root');
+let percentage = document.querySelector('.parsent')
+let plusMinus = document.querySelector('.negative');
+let decimal = document.querySelector('.decimal');
+
+let equal = document.querySelector('.equal');
+let clear = document.querySelector('.clear');
 
 
-let firstValue = "";
-let isfirstValue = false;
-let secondValue = "";
-let isSecondValue = fasle;
-let sign = "";
-let resultValue = 0;
+//displaying the input inside the calculator screen
+numbers.forEach((number) => number.addEventListener('click', (e) => {
+    updateDisplay(e.target.textContent);
+    display.innerText = displayValue;
+}));
+
+//Limit the input display to 9 digit
+updateDisplay = (numBtn) => {
+    if(displayValue.length < 9) {
+        displayValue = firstInput += numBtn;
+    }
+};
+
+//create a function that takes 2 numbers and the operate
+operators.forEach((sign) => sign.addEventListener('click', (e) => {
+    operate(e.target.textContent);
+}))
+
+operate = (sign) => {
+    operator = sign;
+
+    firstInput = secondInput;
+    secondInput = displayValue;
+}
+
+//evaulating the value of inputs
+equal.addEventListener('click', (e) => {
+    if(firstInput != '' && secondInput != ''){
+        calculate();
+        display.innerText = displayValue;
+    }
+})
+
+function calculate() {
+
+    firstInput = Number(firstInput);
+    secondInput = Number(secondInput);
+    
+
+    if(operator === "+") {
+        displayValue =  secondInput + firstInput;
+    }else if(operator === "-") {
+        displayValue =  secondInput - firstInput;
+    }else if(operator === "×") {
+        displayValue =  secondInput * firstInput;
+    }else if(operator === "÷") {
+        displayValue =  secondInput / firstInput;
+    }
+}
 
 
+
+//clear the input of the calculator screen
+clear.addEventListener('click', (e) => {
+    displayValue = "";
+    firstInput = "";
+    secondInput = "";
+    operator = "";
+    display.innerText = 0;
+})
+
+
+//
+root.addEventListener('click', (e) => {
+    squareRoot();
+    function squareRoot() {
+        display.innerText = displayValue * displayValue;
+    }
+})
+
+
+
+//
+percentage.addEventListener('click', (e) => {
+    parcent();
+    function parcent() {
+        display.innerText = (displayValue / 100);
+    }
+})
+
+
+//
+plusMinus.addEventListener('click', (e) => {
+    negative();
+    function negative() {
+        display.innerText = (displayValue * -1);
+    }
+})
 
 
 
